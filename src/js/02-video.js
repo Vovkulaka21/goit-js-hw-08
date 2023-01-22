0e6880fd6ce8e22d775e2d64ab76f534e93b597f
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2023-01-22 16:49:22 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,15 @@ const throttle = require('lodash.throttle');
 const vimeoPlayer = document.querySelector('iframe');
 const player = new Player(vimeoPlayer);
 
-function catchTimeWatch(data) {
-  const saveTime = JSON.stringify(data);
-  localStorage.setItem('videoplayer-current-time', saveTime);
-}
+function catchTime({ seconds }) {
 
-player.on('timeupdate', throttle(catchTimeWatch, 1000));
+  localStorage.setItem('videoplayer-current-time', seconds);
 
-const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+};
 
-player.setCurrentTime(currentTime.seconds);
+
+player.on('timeupdate', throttle(catchTime, 1000));
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+};
