b0adb60d1ac129a43ce524c8b1d72985d7158c50
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2023-01-22 17:08:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,48 +3,41 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 
 const formRef = {
-
   email: document.querySelector('.feedback-form').email,
   message: document.querySelector('.feedback-form').message,
   form: document.querySelector('.feedback-form'),
-
 };
 
-let formData;
+let formData = {};
 
 formRef.form.addEventListener('input', throttle(onInput, 500));
 formRef.form.addEventListener('submit', onSubmitForm);
 
 function onInput(evt) {
-
-  formData = {
-    email: formRef.email.value,
-    message: formRef.message.value,
-  };
+  formData[evt.target.name] = evt.target.value;
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onSubmitForm(event) {
-
   event.preventDefault();
+
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-  formRef.message.textContent = event.target.value;
 
-};
+  console.log(formData);
+}
 
 function populateForm() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
-
-    console.log(JSON.parse(savedMessage));
-
-    const { email, message } = JSON.parse(savedMessage);
-    formRef.email.value = email;
-    formRef.message.textContent = message;
-
+    try {
+      FormData = JSON.parse(savedMessage);
+    } catch (error) {
+      console.log(error.name);
+      console.log(error.message);
+    }
   }
-};
+}
 
 populateForm();
